7f558f13082ff24d23e55ffe702670b5f67fff6d
Hirofumi Nakagawa <hirofumi.nakagawa@mixi.co.jp>
2013-05-08 14:07:14 +0900

    Remove deprecated method call of WebView

--- android/WebViewBridge/src/triaina/webview/WebViewRestoreManager.java
+++ android/WebViewBridge/src/triaina/webview/WebViewRestoreManager.java
@@ -1,52 +1,22 @@
 package triaina.webview;
 
-import java.io.File;
-
-import javax.inject.Inject;
-
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 public class WebViewRestoreManager {
     private static final String TAG = WebViewRestoreManager.class.getSimpleName();
 
-    private static final String EXTRA_WEBVIEW_PICTURE = "_picture";
-    private static final String EXTRA_WEBVIEW_ID = "_webview_id";
-
     private static final String EXTRA_WEBVIEW_STATE = "_webview_state";
-    private static final String WEBVIEW_TMP_FILE = "webview_tmp";
 
-    @Inject
-    private Context mContext;
-
-    @SuppressWarnings("deprecation")
     public boolean restoreWebView(WebViewBridge webViewBridge, Bundle savedInstanceState) {
         if (savedInstanceState == null)
             return false;
 
         try {
-
             Bundle stateBundle = savedInstanceState.getBundle(EXTRA_WEBVIEW_STATE);
             if (stateBundle == null || stateBundle.size() == 0 || webViewBridge.restoreState(stateBundle) == null) {
                 Log.w(TAG, "failed to restore webview state");
                 return false;
-            }
-            if (Build.VERSION.SDK_INT <= 10) {
-                Bundle pictureBundle = savedInstanceState.getBundle(EXTRA_WEBVIEW_PICTURE);
-
-                if (pictureBundle == null) {
-                    Log.w(TAG, "failed to restore webview picture state.");
-                    return false;
-                }
-
-                long id = savedInstanceState.getLong(EXTRA_WEBVIEW_ID);
-                File tmp = new File(mContext.getCacheDir(), WEBVIEW_TMP_FILE + "." + id);
-                if (!webViewBridge.restorePicture(pictureBundle, tmp)) {
-                    Log.w(TAG, "failed to restore webview picture state");
-                    return false;
-                }
             }
         } catch (NullPointerException e) {
             // sometimes, especially repeating fast orientation
@@ -59,7 +29,6 @@ public class WebViewRestoreManager {
         return true;
     }
 
-    @SuppressWarnings("deprecation")
     public void storeWebView(WebViewBridge webViewBridge, Bundle outState, long id) {
         Bundle stateBundle = new Bundle();
         if (webViewBridge.saveState(stateBundle) == null) {
@@ -68,14 +37,5 @@ public class WebViewRestoreManager {
         }
 
         outState.putBundle(EXTRA_WEBVIEW_STATE, stateBundle);
-
-        if (Build.VERSION.SDK_INT <= 10) {
-            Bundle webViewPictureBundle = new Bundle();
-            File tmp = new File(mContext.getCacheDir(), WEBVIEW_TMP_FILE + "." + id);
-            if (!webViewBridge.savePicture(webViewPictureBundle, tmp))
-                Log.w(TAG, "failed to save webview picture");
-            outState.putBundle(EXTRA_WEBVIEW_PICTURE, webViewPictureBundle);
-            outState.putLong(EXTRA_WEBVIEW_ID, id);
-        }
     }
 }
